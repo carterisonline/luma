b38a877e68af054e6e93a5daefaed46fc1a93608
Carter Reeb <carter2k4@pm.me>
2021-05-05 15:49:36 -0500

    Added additional legacy conversions

--- build-legacy.sh
+++ build-legacy.sh
@@ -6,9 +6,12 @@ cp -r ./build ./release
 cd release
 sed -i -- 's/console\.log/println/g' ./*.js
 sed -i -- 's/createCanvas/\/\/\ createCanvas/g' ./*.js
-sed -ri -- 's/[^\.]push/pushMatrix/g' ./*.js
-sed -ri -- 's/[^\.]pop/popMatrix/g' ./*.js
+sed -ri -- 's/[^\.]push/\ pushMatrix/g' ./*.js
+sed -ri -- 's/[^\.]pop/\ popMatrix/g' ./*.js
 sed -ri -- 's/print[^l]\s*?/println\(/g' ./*.js
 sed -ri -- 's/new\s*?Array\s*?\(\)/new\ Array\(0\)/g' ./*.js
 sed -i -- 's/createVector/new\ PVector/g' ./*.js
 sed -i -- 's/function\sdraw/\nsetup\(\);\nfunction\ draw/g' ./*.js
+sed -i -- 's/windowWidth/width/g' ./*.js
+sed -i -- 's/windowHeight/height/g' ./*.js
+sed -i -- 's/resizeCanvas/\/\/\ resizeCanvas/g' ./*.js
